c4920c86c2b4273d1cf42d7a18315e4ffce9743d
Érico Andrei <erico@simplesconsultoria.com.br>
2013-06-03 17:12:47 -0300

    Add Image field to ExternalBlogEntry

--- src/cartacapital/portal/externalcontent/content/blog_entry.py
+++ src/cartacapital/portal/externalcontent/content/blog_entry.py
@@ -1,7 +1,12 @@
-# -*- coding: utf-8 -*-
+    # -*- coding: utf-8 -*-
 from plone.dexterity.content import Item
 from zope.interface import implements
 from zope.interface import Interface
+
+
+def has_image(blog_entry):
+    image = blog_entry.image
+    return (image and image.getSize())
 
 
 class IExternalBlogEntry(Interface):
@@ -11,3 +16,21 @@ class IExternalBlogEntry(Interface):
 
 class ExternalBlogEntry(Item):
     implements(IExternalBlogEntry)
+
+    def image_thumb(self):
+        ''' Return a thumbnail '''
+        if not has_image(self):
+            return None
+        view = self.unrestrictedTraverse('@@images')
+        return view.scale(fieldname='image',
+                          scale='thumb').index_html()
+
+    def tag(self, scale='thumb', css_class='tileImage', **kw):
+        ''' Return a tag to the image '''
+        if not (has_image(self)):
+            return ''
+        view = self.unrestrictedTraverse('@@images')
+        return view.tag(fieldname='image',
+                        scale=scale,
+                        css_class=css_class,
+                        **kw)
